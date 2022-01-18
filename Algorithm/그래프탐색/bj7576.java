274b33cad1184ba4e8a1ae85993b21e3d80b9e9e
uuuouuo <som0ng@naver.com>
2022-01-18 20:38:10 +0900

    220118

---
+++ Algorithm/그래프탐색/bj7576.java
@@ -0,0 +1,79 @@
+package 그래프탐색;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/** 토마토 */
+public class bj7576 {
+
+	static class Point {
+		int r, c, cnt;
+
+		public Point(int r, int c, int cnt) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.cnt = cnt;
+		}
+	}
+
+	static int N, M, box[][], ans;
+	static Queue<Point> q;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		M = sc.nextInt();
+		N = sc.nextInt();
+
+		box = new int[N][M];
+		q = new LinkedList<Point>();
+		ans = 0;
+
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				box[r][c] = sc.nextInt();
+				if (box[r][c] == 1)
+					q.add(new Point(r, c, 0));
+
+			}
+		}
+
+		bfs();
+
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < M; c++) {
+				if (box[r][c] == 0) {
+					System.out.println(-1);
+					System.exit(0);
+				}
+			}
+		}
+
+		System.out.println(ans);
+
+	}
+
+	static int[] dr = { -1, 0, 1, 0 }, dc = { 0, 1, 0, -1 };
+
+	public static void bfs() {
+
+		while (!q.isEmpty()) {
+			Point cur = q.poll();
+			int r = cur.r, c = cur.c, cnt = cur.cnt;
+			ans = Math.max(cnt, ans);
+
+			for (int d = 0; d < 4; d++) {
+				int nr = r + dr[d], nc = c + dc[d];
+
+				if (nr < 0 || nc < 0 || nr >= N || nc >= M || box[nr][nc] != 0)
+					continue;
+
+				q.add(new Point(nr, nc, cnt + 1));
+				box[nr][nc] = 1;
+
+			}
+		}
+	}
+}
---
+++ Algorithm/그래프탐색/bj9372.java
@@ -0,0 +1,66 @@
+package 그래프탐색;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+/** 상근이의 여행 : 그냥 최소 간선수를 구하면 문제였음 (N -1) */
+public class bj9372 {
+
+	static int N, M, ans;
+	static List<Integer> list[];
+	static boolean visited[];
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+
+			N = sc.nextInt();
+			M = sc.nextInt();
+			list = new ArrayList[N + 1]; // 배열로 선언 !
+			visited = new boolean[N + 1];
+
+			for (int i = 1; i <= N; i++) { // 인접 리스트 행렬 생성 ㄴ|｀O′|ㄱ !!!
+				list[i] = new ArrayList<>();
+			}
+
+			for (int i = 0; i < M; i++) {
+				int from = sc.nextInt();
+				int to = sc.nextInt();
+				list[from].add(to);
+				list[to].add(from);
+			}
+
+			bfs(1, 0);
+		}
+
+	}
+
+	static Queue<Integer> q;
+
+	private static void bfs(int start, int cnt) { // 리스트를 이용한 bfs.
+		q = new LinkedList<Integer>();
+		q.add(start); // 시작점 큐에 담기.
+		visited[start] = true; // 시작점 방문 체크.
+
+		while (!q.isEmpty()) {
+			int cur = q.poll(); // 큐에 담긴 숫자 꺼내기.
+			for (int i = 0; i < list[cur].size(); i++) { // 현재 숫자의 접점만 검사 ㄴ|｀O′|ㄱ !!!
+				int next = list[cur].get(i); // 현재 숫자의 접점 ㄴ|｀O′|ㄱ !!!
+
+				if (visited[next])
+					continue; // 만약 이미 방문했으면 패스.
+				visited[next] = true; // 아니면 방문체크하고
+				cnt++;
+				q.add(next); // 카운팅, 다음 숫자 큐에 담기.
+			}
+		}
+
+		System.out.println(cnt);
+
+	}
+
+}
