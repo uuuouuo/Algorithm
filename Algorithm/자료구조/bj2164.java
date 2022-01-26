375969b711467d27d8a65bd43ed31be1ae219d8a
uuuouuo <som0ng@naver.com>
2022-01-26 19:42:06 +0900

    [Algorithm] 220126

---
+++ Algorithm/자료구조/bj2164.java
@@ -0,0 +1,28 @@
+package 자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class bj2164 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		for (int i = 1; i <= N; i++) {
+			q.add(i);
+		}
+		
+		while(q.size() != 1){			
+			q.poll();
+			q.add(q.poll());			
+		}
+		System.out.println(q.poll());
+	}
+
+}
