1be8f5216abaacedae4f6e56f48bd1cfcba2363d
ì <som0ng@naver.com>
2022-02-04 18:28:21 +0900

    [Algorithm] 220204

---
+++ Algorithm/그리디/bj1758.java
@@ -0,0 +1,33 @@
+package 그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class bj1758 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		Integer tip[] = new Integer[N];
+		for (int i = 0; i < N; i++) {
+			tip[i] = Integer.parseInt(br.readLine());
+		}
+		Arrays.sort(tip, Collections.reverseOrder()); // int형 안돼
+
+		long sum = 0;
+		for (int i = 0; i < N; i++) {
+			int tmp = tip[i] - i;
+
+			if(tmp > 0)
+				sum += tmp;
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
