303a8288625ddeeb45066de7710c59a2c679cc28
uuuouuo <som0ng@naver.com>
2022-04-21 21:59:18 +0900

    220421

---
+++ Algorithm/프로그래머스/행렬의곱셈.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        // arr1[0][0,1,2] * arr2[0,1,2][0] 
+        // arr1의 행 길이 = arr2의 열 길이
+
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr2[0].length; j++) {
+                int tmp = 0;
+                for(int k = 0; k < arr2.length; k++) { 
+                    tmp += arr1[i][k] * arr2[k][j]; 
+                }
+                answer[i][j] = tmp;
+            }
+        }
+        return answer;
+    }
+}
