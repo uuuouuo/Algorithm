43096ad93165cf04f698d1185c375ab1be96c16d
uuuouuo <som0ng@naver.com>
2022-05-09 22:22:58 +0900

    220509

---
+++ Algorithm/프로그래머스/가운데글자가져오기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int length = s.length();
+        int half = length / 2;
+
+        if(length % 2 == 0)            
+            answer = s.substring(half - 1, half + 1);
+        else 
+            answer = String.valueOf(s.charAt(half));
+        return answer;
+    }
+}
