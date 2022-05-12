2476e261355072f6ed3a040a5cd46baacc7e28fd
uuuouuo <som0ng@naver.com>
2022-05-12 16:22:57 +0900

    220512

---
+++ Algorithm/프로그래머스/예상대진표.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    static int answer;
+
+    public int solution(int n, int a, int b) {
+        answer = 1;
+        size = n / 2;
+        go(n / 2, a, b);
+
+        return answer;
+    }
+
+    static int size;
+
+    static void go(int n, int a, int b) {
+        if (!isOk(n, a, b)) {
+            go(get(n, a), a, b);
+        }
+
+        while (size != 1) {
+            answer++;
+            size = size / 2;
+        }
+    }
+
+    static boolean isOk(int n, int a, int b) {
+        if ((a <= n && b > n) || (a > n && b <= n))
+            return true;
+        return false;
+    }
+
+    static int get(int n, int a) {
+        size = size / 2;
+
+        if (a <= n)
+            return n - size;
+        return n + size;
+    }
+
+}
