45dfb33d48694e18c14007fb3a49d93ceb5d863c
uuuouuo <som0ng@naver.com>
2022-05-21 12:33:57 +0900

    0521 완주하지못한선수

---
+++ 프로그래머스/완주하지못한선수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < participant.length; i++) {
+            String p = participant[i];
+            if (!map.containsKey(p))
+                map.put(p, 1);
+            else
+                map.replace(p, map.get(p) + 1);
+        }
+
+        String answer = "";
+        for (int i = 0; i < completion.length; i++) {
+            map.replace(completion[i], map.get(completion[i]) - 1);
+        }
+
+        for (Entry<String, Integer> e : map.entrySet()) {
+            if (e.getValue() != 0)
+                answer = e.getKey();
+        }
+
+        return answer;
+    }
+}
