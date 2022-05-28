1626f8d6a2bf1c0e2cb003d0548d8fe3ce53e6a9
uuuouuo <som0ng@naver.com>
2022-05-28 12:54:31 +0900

    0528 네트워크

---
+++ 프로그래머스/네트워크.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    static List<Integer> list[];
+    static boolean[] visited;
+    static int answer;
+
+    public int solution(int n, int[][] computers) {
+        answer = 0;
+        list = new ArrayList[n];
+        visited = new boolean[n];
+        init(n, computers);
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(i);
+            }
+        }
+
+        return answer;
+    }
+
+    static void init(int n, int[][] computers) {
+        for (int i = 0; i < n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (computers[i][j] == 1 && i != j)
+                    list[i].add(j);
+            }
+        }
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+
+        for (int l : list[node]) {
+            if (visited[l])
+                continue;
+            dfs(l);
+        }
+    }
+}
