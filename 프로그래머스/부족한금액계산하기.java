6e2ba98694ca98f833c19ee9091ec49e44ffd4b9
uuuouuo <som0ng@naver.com>
2022-06-02 22:36:25 +0900

    0602 부족한 금액 계산하기

---
+++ 프로그래머스/부족한금액계산하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        for (int i = 0; i < count; i++)
+            answer += price * (i + 1);
+
+        if (answer - money <= 0)
+            return 0;
+        return Math.abs(answer - money);
+    }
+}
