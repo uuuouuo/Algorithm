b9f4bff4d5b5c857bdc985cb45d84bd5673e32ec
uuuouuo <som0ng@naver.com>
2022-07-04 21:12:45 +0900

    0704 가장큰수

---
+++ 프로그래머스/가장큰수.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    static int N;
+    static String[] input;
+
+    public String solution(int[] numbers) {
+        String answer = "";
+        N = numbers.length;
+        input = new String[N];
+        toString(numbers);
+
+        Arrays.sort(input, (o1, o2) -> {
+            return (o2 + o1).compareTo(o1 + o2); // 더 큰수 반환: 내림차순
+        });
+
+        // for(String i : input)
+        // System.out.println(i);
+
+        for (String i : input)
+            answer += i;
+        if (answer.startsWith("0"))
+            return "0";
+        return answer;
+    }
+
+    static void toString(int[] numbers) {
+        for (int i = 0; i < N; i++)
+            input[i] = String.valueOf(numbers[i]);
+    }
+}
