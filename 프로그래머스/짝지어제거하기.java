ab813260a97068e13900403f84ecf8e0c9dc1dd2
uuuouuo <som0ng@naver.com>
2022-07-06 22:03:52 +0900

    0706 짝지어제거하기

---
+++ 프로그래머스/짝지어제거하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (!st.isEmpty() && st.peek() == c)
+                st.pop();
+            else
+                st.push(c);
+        }
+        return st.isEmpty() ? 1 : 0;
+    }
+}
