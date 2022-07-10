94741210888306fa886f5070295fd42bb699235c
uuuouuo <som0ng@naver.com>
2022-07-10 14:58:23 +0900

    0710 문자열 내 p와y의 개수

---
+++ 프로그래머스/문자열내p와y의개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    boolean solution(String s) {
+        int pCnt = 0;
+        int yCnt = 0;
+
+        s = s.toLowerCase();
+        for (char c : s.toCharArray()) {
+            if (c == 'p')
+                pCnt++;
+            if (c == 'y')
+                yCnt++;
+            // System.out.println(c);
+        }
+        if (pCnt == yCnt)
+            return true;
+        return false;
+    }
+}
