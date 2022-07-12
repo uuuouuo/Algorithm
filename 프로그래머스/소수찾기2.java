440664e1dcccf830b86f52cf65009c1b66903ca2
uuuouuo <som0ng@naver.com>
2022-07-12 20:37:18 +0900

    0712 소수찾기2

---
+++ 프로그래머스/소수찾기2.java
@@ -0,0 +1,21 @@
+class Solution {
+    static int answer;
+
+    public int solution(int n) {
+        answer = 0;
+
+        for (int i = 2; i <= n; i++)
+            if (isPrime(i))
+                answer++;
+        return answer;
+    }
+
+    static boolean isPrime(int n) {
+        if (n == 2)
+            return true;
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0)
+                return false;
+        return true;
+    }
+}
