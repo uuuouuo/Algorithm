2ef510493c226920ab9e410d1d330f1cddb4c436
uuuouuo <som0ng@naver.com>
2022-07-18 22:13:01 +0900

    0718 새로운 불면증 치료법

---
+++ SWEA/SWEA_새로운불면증치료법.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            String strN = br.readLine();
+            int intN = Integer.parseInt(strN);
+
+            int k = 1;
+            int cnt = 0; // 모든 수 체크했는지
+            boolean[] check = new boolean[10];
+            while (true) {
+                int size = strN.length();
+                for (int i = 0; i < size; i++) {
+                    int idx = Integer.parseInt(strN.substring(i, i + 1));
+                    if (!check[idx]) {
+                        check[idx] = true;
+                        cnt++;
+                    }
+                }
+
+                if (cnt == 10)
+                    break;
+
+                k++;
+                strN = Integer.toString(k * intN);
+            }
+            System.out.println("#" + t + " " + strN);
+        }
+
+    }
+
+}
