e2071c9225b0e4abcaf7113029070ae9e744bb0f
uuuouuo <som0ng@naver.com>
2022-08-25 18:17:38 +0900

    멀쩡한 사각형

---
+++ 프로그래머스/멀쩡한사각형.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    // 1. 최대 공약수 구하기
+    // 2. 공식에 넣어 구하기
+    public long solution(int w, int h) {
+        long small = 0, big = 0;
+        if (w < h) {
+            small = (long) w;
+            big = (long) h;
+        } else {
+            small = (long) h;
+            big = (long) w;
+        }
+
+        long gcd = gcd(small, big);
+        return small * big - (small / gcd + big / gcd - 1) * gcd;
+    }
+
+    static long gcd(long small, long big) {
+        long tmp = 0;
+        while (small > 0) {
+            tmp = big % small;
+            big = small;
+            small = tmp;
+        }
+
+        return big;
+    }
+}
