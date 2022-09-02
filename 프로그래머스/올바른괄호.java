1abebd3f53b674a0df88699444a21eb9a4757ef8
uuuouuo <som0ng@naver.com>
2022-09-02 20:40:34 +0900

    올바른 괄호

---
+++ 프로그래머스/올바른괄호.java
@@ -0,0 +1,24 @@
+class Solution {
+    boolean solution(String s) {
+        int n = s.length();
+        int tmp = 0;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(')
+                tmp++;
+            else
+                tmp--;
+
+            if (tmp < 0)
+                break;
+        }
+
+        if (tmp == 0)
+            return true;
+        else
+            return false;
+    }
+}
+
+public class 올바른괄호 {
+
+}
