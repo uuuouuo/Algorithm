55f9937412433c16dcd1e9bb888e1135514fd72d
uuuouuo <som0ng@naver.com>
2022-09-04 23:20:03 +0900

    같은 숫자는 싫어

---
+++ 프로그래머스/같은숫자는싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public List<Integer> solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            // 비어있거나(맨처음일 경우), 앞 숫자와 다르면 넣기
+            if (st.isEmpty() || st.peek() != arr[i]) {
+                st.push(arr[i]);
+                answer.add(arr[i]);
+            }
+        }
+
+        return answer;
+    }
+}
