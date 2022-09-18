57cf424127837c4a1f3cb0723590cc01b6dd9f26
uuuouuo <som0ng@naver.com>
2022-09-18 23:14:06 +0900

    피보나치수

---
+++ 프로그래머스/피보나치수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            if (2 > i)
+                arr[i] = i % 1234567;
+            else
+                arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+        }
+        answer = arr[n];
+        return answer;
+    }
+}
