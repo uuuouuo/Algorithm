9a781c93ca5f11da9a0a916994c67ae1aefa300c
uuuouuo <som0ng@naver.com>
2022-09-23 21:44:41 +0900

    n진수 게임

---
+++ 프로그래머스/n진수게임.java
@@ -0,0 +1,19 @@
+// 문제 대충 봄.. (n의 범위!)
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder sb = new StringBuilder();
+        int max = t * m; // t * m => 이만큼 길이까지 구하기
+        int num = 0;
+        while (max > sb.toString().length()) {
+            // 10진수 -> n진수
+            sb.append(Integer.toString(num++, n));
+        }
+
+        String answer = "";
+        for (int i = p - 1; i < max; i += m) {
+            answer += sb.toString().substring(i, i + 1);
+        }
+        return answer.toUpperCase();
+    }
+
+}
