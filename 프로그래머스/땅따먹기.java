c12b853b30a7af4054bd93a2139a91c4c03a3771
uuuouuo <som0ng@naver.com>
2022-09-24 23:04:11 +0900

    땅따먹기

---
+++ 프로그래머스/땅따먹기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    int solution(int[][] land) {
+        int n = land.length;
+        for(int i = 1; i < n; i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
+        }
+        
+        
+        return Arrays.stream(land[n-1]).max().getAsInt();
+    }
+}
