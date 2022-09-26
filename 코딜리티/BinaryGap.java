5c73a21464dacfe33b8c725978f2635018a699a2
uuuouuo <som0ng@naver.com>
2022-09-26 18:32:23 +0900

    BinaryGap

---
+++ 코딜리티/BinaryGap.java
@@ -0,0 +1,22 @@
+package 코딜리티;
+
+class Solution {
+    public int solution(int N) {
+        String s = Integer.toString(N, 2);
+        int answer = 0;
+        int pre = -1;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
+                if (pre == -1) {
+                    pre = i;
+                    continue;
+                }
+                int gap = i - pre - 1;
+                if (answer < gap)
+                    answer = gap;
+                pre = i;
+            }
+            return answer;
+        }
+    }
+}
