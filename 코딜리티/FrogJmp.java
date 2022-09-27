659a0c12293f5e94e90968b691d3c25b5e31550f
uuuouuo <som0ng@naver.com>
2022-09-27 22:27:28 +0900

    Frog Jmp

---
+++ 코딜리티/FrogJmp.java
@@ -0,0 +1,14 @@
+package 코딜리티;
+
+class Solution {
+    public int solution(int X, int Y, int D) {
+        int cnt = 0;
+        int tmp = 0;
+        while (Y > tmp) {
+            // System.out.println(D);
+            cnt++;
+            tmp += D;
+        }
+        return cnt;
+    }
+}
