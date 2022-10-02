0eb221cf2186f07e3b14813533769e8f5a044656
uuuouuo <som0ng@naver.com>
2022-10-02 22:50:03 +0900

    CyclicRotation

---
+++ 코딜리티/CyclicRotation.java
@@ -0,0 +1,25 @@
+package 코딜리티;
+
+class Solution {
+    public int[] solution(int[] A, int K) {
+        for (int i = 0; i < K; i++) {
+            swap(A);
+        }
+
+        return A;
+    }
+
+    static void swap(int[] A) {
+        int tmp = A[A.length - 1];
+        for (int i = A.length - 1; i >= 0; i--) {
+            if (i == 0)
+                A[i] = tmp;
+            else
+                A[i] = A[i - 1];
+        }
+
+        // for(int i=0; i<A.length; i++)
+        // System.out.print(A[i]);
+        // System.out.println();
+    }
+}
