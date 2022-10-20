2bf25bd22b339ae76d0bd4a4f5b4dd047163e1ff
uuuouuo <som0ng@naver.com>
2022-10-20 20:54:01 +0900

    혼자놀기의 달인

---
+++ 프로그래머스/혼자놀기의달인.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(int[] cards) {
+        int answer = 0;
+        boolean[] check;
+
+        for (int i = 0; i < cards.length; i++) {
+            check = new boolean[cards.length];
+            int box1 = go(cards, check, i); // 1번 상자 그룹 개수
+            boolean[] clone;
+            for (int j = 0; j < cards.length; j++) {
+                clone = check.clone();
+                if (!check[j]) {
+                    int res = box1 * go(cards, clone, j); // box1 개수 * box2 개수
+                    if (res > answer)
+                        answer = res;
+                }
+            }
+        }
+        return answer;
+    }
+
+    static int go(int[] cards, boolean[] check, int n) { // 상자 열어보기
+        int cnt = 0; // 열어본 상자 개수
+        while (!check[n]) {
+            check[n] = true;
+            n = cards[n] - 1;
+            cnt++;
+        }
+        return cnt;
+    }
+}
