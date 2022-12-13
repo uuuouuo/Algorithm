fb8391e4c38e84891a12ba196973cb4ad67e9aac
uuuouuo <som0ng@naver.com>
2022-12-13 19:09:12 +0900

    롤케이크자르기

---
+++ 프로그래머스/롤케이크자르기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {        
+        int answer = 0; // 토핑 종류가 같게 되는 모든 경우의 수
+        Map<Integer, Integer> bro = new HashMap<>();
+    
+        for(int i=0; i<topping.length; i++) {
+            int cur = topping[i];
+            if(!bro.containsKey(cur))
+                bro.put(cur, 1);
+            else bro.put(cur, bro.get(cur)+1);
+        }
+        
+        // 슬라이딩 윈도우 !!!
+        int idx = 0;
+        Map<Integer, Integer> me = new HashMap<>();
+        while(idx <= topping.length-1) {
+            int cur = topping[idx];
+            int curN = bro.get(cur)-1;
+
+            if(curN == 0) bro.remove(cur); // 동생 토핑 -1
+            else bro.put(cur, curN);
+            
+            if(!me.containsKey(cur)) me.put(cur, 1);
+            else me.put(cur, me.get(cur)+1);
+            
+            if(me.size() == bro.size()) answer++;
+            
+            idx++;
+        }
+        return answer;
+    }
+}
