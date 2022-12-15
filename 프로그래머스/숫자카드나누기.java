c8f37456c4cba4e292e54ca408a2a0b5c8cbc971
uuuouuo <som0ng@naver.com>
2022-12-15 23:55:45 +0900

    숫자카드나누기

---
+++ 프로그래머스/숫자카드나누기.java
@@ -0,0 +1,22 @@
+class Solution {
+    static int answer;
+    public int solution(int[] arrayA, int[] arrayB) {
+        answer = 0;
+        go(arrayA, arrayB);
+        go(arrayB, arrayA);
+        return answer;
+    }
+    
+    static void go(int[] arr1, int[] arr2) {       
+        // 2 ~ 가장 작은 원소까지 조건 확인
+        l: for(int i=2; i<=arr1[0]; i++) {
+            for(int n:arr1) {
+                if(n%i != 0) continue l;
+            }            
+            for(int n:arr2) {
+                if(n%i == 0) continue l;
+            }
+            if(answer < i) answer = i;
+        }
+    }
+}
