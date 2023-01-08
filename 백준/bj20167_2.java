7d9df398d9915baf338425afd11e08fad0e70cd8
uuuouuo <som0ng@naver.com>
2023-01-08 22:59:21 +0900

    꿈틀꿈틀 호석 에벌레 - 가능성 (재귀 풀이

--- 백준/bj20167_2.java
+++ 백준/bj20167_2.java
@@ -7,17 +7,45 @@ import java.util.StringTokenizer;
 
 public class bj20167_2 {
 
-    static int N, K, A[], E[];
+    static int N, K, foods[], answer;
     public static void main(String[] args) throws IOException {
         input();
-
+        go(0, 0, 0);
+        print();
     }
 
-    private static void go(int idx, int sum) {
-        if(N == idx) return; // 끝까지 오면 그만
+    private static void print() {
+        System.out.println(answer);
+    }
 
-        if(K > sum+A[idx]) go(idx+1, sum+(K-A[idx]))
+    /**
+     * 완전탐색 - 모든 경우의 수로 구하기
+     *
+     * @param idx : foods[]의 인덱스
+     * @param fSum : 현재까지 먹은 먹이의 만족도 합
+     * @param eSum : 현재까지 얻은 탈피 에너지
+     */
+    private static void go(int idx, int fSum, int eSum) {
+        // foods[] 마지막 인덱스까지 왔다면
+        // 이전 탈피에너지(answer)보다 현재 탈피에너지(eSum)가 더 크면 갱신
+        if(idx == N) {
+            if(answer < eSum) answer = eSum;
+            return;
+        }
 
+        // 1. 현재 먹이를 먹지 않고 그냥 지나가는 경우
+            // 먹이 만족도는 0이 됨
+        go(idx+1, 0, eSum);
+        // 2. 현재 먹이를 먹어도 최소 만족도보다 작은 경우
+            // 먹이 만족도 증가
+            // 탈피 에너지는 그대로
+        if(K > fSum + foods[idx]) go(idx+1, fSum+foods[idx], eSum);
+        // 3. 현재 먹이를 먹으면 최소 만족도보다 큰 경우
+            // 먹이 만족도는 0이 됨
+            // K보다 큰 만큼 탈피에너지가 됨
+        else go(idx+1, 0, eSum+((fSum+foods[idx]) - K));
+
+        return;
     }
 
     private static void input() throws IOException {
@@ -26,12 +54,12 @@ public class bj20167_2 {
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        A = new int[N+1];
-        E = new int[N+1];
+        foods = new int[N];
+        answer = 0;
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            foods[i] = Integer.parseInt(st.nextToken());
         }
     }
 
